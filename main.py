04119e5cc7d8064011d2aacd886a33e835ffeab1
hartofwave <hartofwave@linux-pd6p.Home>
2017-07-16 15:48:29 +0100

    updates

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(‘home.html’)
+    return render_template('index.html')
 
 
-if __name__ == '__main__':
+@app.route('/login', methods = ['POST'])
+def signup():
+    user = request.form['user_name']
+    print("The email address is '" + user + "'")
+    return render_template('question_page.html')
